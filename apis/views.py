5786c78721da40496c5c0805d7135ff102a9df89
zaahiir <smartxoft@outlook.com>
2025-06-19 19:06:44 +0530

    feat
    up

--- apis/views.py
+++ apis/views.py
@@ -23,7 +23,7 @@ from django.shortcuts import get_object_or_404
 from django.utils import timezone
 
 logger = logging.getLogger(__name__)
-
+ 
 
 class UserViewSet(viewsets.ViewSet):
     @action(detail=False, methods=['post'])
